7e351117f8edcdba1903b1326513af73d6c107ff
Nirush4 <nirushvj@gmail.com>
2025-03-12 14:04:18 +0100

    feat: change folder structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
+  appType: 'mpa',
+  base: '',
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        profile: resolve(__dirname, './profile/index.html'),
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
